c26e269f2a352a1cf93b76c54a3123ba17a9a589
manne ohrstrom <manne@shotgunsoftware.com>
2012-09-23 12:19:19 +0100

    code cleanup + pyside import

--- engine.py
+++ engine.py
@@ -15,13 +15,12 @@ import tank
 from pyfbsdk import FBMessageBox
 
 
-CONSOLE_OUTPUT_WIDTH = 120
-
 class MotionBuilderEngine(tank.platform.Engine):
+    
     def init_engine(self):
         self.log_debug("%s: Initializing..." % self)
 
-        # now check that there is a location on disk which corresponds to the context
+        # now check that there is a location on disk which 
         if self.context.entity:
             # context has an entity
             locations = self.tank.paths_from_entity(self.context.entity["type"],
@@ -32,15 +31,57 @@ class MotionBuilderEngine(tank.platform.Engine):
                                                     self.context.project["id"])
         else:
             # must have at least a project in the context to even start!
-            raise tank.TankError("The nuke engine needs at least a project in the context "
+            raise tank.TankError("The Tank engine needs at least a project in the context "
                                  "in order to start! Your context: %s" % self.context)
 
         # make sure there are folders on disk
         if len(locations) == 0:
-            raise tank.TankError("No folders on disk are associated with the current context. The Nuke "
+            raise tank.TankError("No folders on disk are associated with the current context. The "
                             "engine requires a context which exists on disk in order to run "
                             "correctly.")
 
+        # import pyside QT UI libraries
+        self._init_pyside()
+
+
+
+
+    def _init_pyside(self):
+        """
+        Handles the pyside init
+        """
+        
+        # first see if pyside is already present - in that case skip!
+        try:
+            from PySide import QtGui
+        except:
+            # fine, we don't expect pyside to be present just yet
+            self.log_debug("PySide not detected - Tank will add it to the setup now...")
+        else:
+            # looks like pyside is already working! No need to do anything
+            self.log_debug("PySide detected - Tank will use the existing version.")
+            return
+        
+        if sys.platform == "win32":
+            pyside_path = os.path.join(self.disk_location, "resources","pyside112_py26_qt470_win64", "python")
+            sys.path.append(pyside_path)   
+            dll_path = os.path.join(self.disk_location, "resources","pyside112_py26_qt470_win64", "lib")
+            path = os.environ.get("PATH", "")
+            path += ";%s" % dll_path
+            os.environ["PATH"] = path
+                     
+        else:
+            self.log_error("Unknown platform - cannot initialize PySide!")
+        
+        # now try to import it
+        try:
+            from PySide import QtGui
+        except Exception, e:
+            self.log_error("PySide could not be imported! Tank Apps using pyside will not "
+                           "operate correctly! Error reported: %s" % e)
+
+
+    def post_app_init(self):
         import tk_motionbuilder
         self._menu_generator = tk_motionbuilder.MenuGenerator(self)
         self._menu_generator.create_menu()
@@ -50,7 +91,8 @@ class MotionBuilderEngine(tank.platform.Engine):
         self._menu_generator.destroy_menu()
 
     def log_debug(self, msg):
-        print msg
+        if self.get_setting("debug_logging", False):
+            print msg
 
     def log_info(self, msg):
         print msg
