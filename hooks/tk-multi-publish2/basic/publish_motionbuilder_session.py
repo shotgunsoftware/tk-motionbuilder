b19b247dca5dc2846757758f8c9d97d6111cc21f
Luis Gomez Larez <luis.gomez.larez@autodesk.com>
2017-10-22 21:23:54 -0700

    44077 First pass at support in tk-config-default2 with tk-multi-publish2 engine hooks

---
+++ hooks/tk-multi-publish2/basic/publish_motionbuilder_session.py
@@ -0,0 +1,420 @@
+﻿# Copyright (c) 2017 Shotgun Software Inc.
+# 
+# CONFIDENTIAL AND PROPRIETARY
+# 
+# This work is provided "AS IS" and subject to the Shotgun Pipeline Toolkit 
+# Source Code License included in this distribution package. See LICENSE.
+# By accessing, using, copying or modifying this work you indicate your 
+# agreement to the Shotgun Pipeline Toolkit Source Code License. All rights 
+# not expressly granted therein are reserved by Shotgun Software Inc.
+
+import os
+import pprint
+import sgtk
+
+from pyfbsdk import FBApplication
+
+mb_app = FBApplication()
+
+HookBaseClass = sgtk.get_hook_baseclass()
+
+
+class MotionBuilderSessionPublishPlugin(HookBaseClass):
+    """
+    Plugin for publishing an open motion builder session.
+    """
+
+    @property
+    def icon(self):
+        """
+        Path to an png icon on disk
+        """
+
+        # look for icon one level up from this hook's folder in "icons" folder
+        return os.path.join(
+            self.disk_location,
+            os.pardir,
+            "icons",
+            "publish.png"
+        )
+
+    @property
+    def name(self):
+        """
+        One line display name describing the plugin
+        """
+        return "Publish to Shotgun"
+
+    @property
+    def description(self):
+        """
+        Verbose, multi-line description of what the plugin does. This can
+        contain simple html for formatting.
+        """
+
+        loader_url = "https://support.shotgunsoftware.com/hc/en-us/articles/219033078"
+
+        return """
+        Publishes the file to Shotgun. A <b>Publish</b> entry will be
+        created in Shotgun which will include a reference to the file's current
+        path on disk. Other users will be able to access the published file via
+        the <b><a href='%s'>Loader</a></b> so long as they have access to
+        the file's location on disk.
+
+        If the session has not been saved, validation will fail and a button
+        will be provided in the logging output to save the file.
+
+        <h3>File versioning</h3>
+        If the filename contains a version number, the process will bump the
+        file to the next version after publishing.
+
+        The <code>version</code> field of the resulting <b>Publish</b> in
+        Shotgun will also reflect the version number identified in the filename.
+        The basic worklfow recognizes the following version formats by default:
+
+        <ul>
+        <li><code>filename.v###.ext</code></li>
+        <li><code>filename_v###.ext</code></li>
+        <li><code>filename-v###.ext</code></li>
+        </ul>
+
+        After publishing, if a version number is detected in the file, the file
+        will automatically be saved to the next incremental version number.
+        For example, <code>filename.v001.ext</code> will be published and copied
+        to <code>filename.v002.ext</code>
+
+        If the next incremental version of the file already exists on disk, the
+        validation step will produce a warning, and a button will be provided in
+        the logging output which will allow saving the session to the next
+        available version number prior to publishing.
+
+        <br><br><i>NOTE: any amount of version number padding is supported.</i>
+
+        <h3>Overwriting an existing publish</h3>
+        A file can be published multiple times however only the most recent
+        publish will be available to other users. Warnings will be provided
+        during validation if there are previous publishes.
+        """ % (loader_url,)
+        # TODO: add link to workflow docs
+
+    @property
+    def settings(self):
+        """
+        Dictionary defining the settings that this plugin expects to receive
+        through the settings parameter in the accept, validate, publish and
+        finalize methods.
+
+        A dictionary on the following form::
+
+            {
+                "Settings Name": {
+                    "type": "settings_type",
+                    "default": "default_value",
+                    "description": "One line description of the setting"
+            }
+
+        The type string should be one of the data types that toolkit accepts as
+        part of its environment configuration.
+        """
+
+        # inherit the settings from the base publish plugin
+        base_settings = super(MotionBuilderSessionPublishPlugin, self).settings or {}
+
+        # settings specific to this class
+        mobu_publish_settings = {
+            "Publish Type": {
+                "type": "shotgun_publish_type",
+                "default": "Motion Builder FBX",
+                "description": "SG publish type to associate publishes with."
+            },
+            "Publish Template": {
+                "type": "template",
+                "default": None,
+                "description": "Template path for published work files. Should"
+                               "correspond to a template defined in "
+                               "templates.yml.",
+            },
+        }
+
+        # update the base settings
+        base_settings.update(mobu_publish_settings)
+
+        return base_settings
+
+    @property
+    def item_filters(self):
+        """
+        List of item types that this plugin is interested in.
+
+        Only items matching entries in this list will be presented to the
+        accept() method. Strings can contain glob patters such as *, for example
+        ["motionbuilder.*", "file.motionbuilder"]
+        """
+        return ["motionbuilder.fbx"]
+
+    def accept(self, settings, item):
+        """
+        Method called by the publisher to determine if an item is of any
+        interest to this plugin. Only items matching the filters defined via the
+        item_filters property will be presented to this method.
+
+        A publish task will be generated for each item accepted here. Returns a
+        dictionary with the following booleans:
+
+            - accepted: Indicates if the plugin is interested in this value at
+                all. Required.
+            - enabled: If True, the plugin will be enabled in the UI, otherwise
+                it will be disabled. Optional, True by default.
+            - visible: If True, the plugin will be visible in the UI, otherwise
+                it will be hidden. Optional, True by default.
+            - checked: If True, the plugin will be checked in the UI, otherwise
+                it will be unchecked. Optional, True by default.
+
+        :param settings: Dictionary of Settings. The keys are strings, matching
+            the keys returned in the settings property. The values are `Setting`
+            instances.
+        :param item: Item to process
+
+        :returns: dictionary with boolean keys accepted, required and enabled
+        """
+
+        path = _session_path()
+
+        if not path:
+            # the session has not been saved before (no path determined).
+            # provide a save button. the session will need to be saved before
+            # validation will succeed.
+            self.logger.warn(
+                "The Motion Builder session has not been saved.",
+                extra=_get_save_as_action()
+            )
+
+        accepted = True
+        publisher = self.parent
+        template_name = settings["Publish Template"].value
+
+        publish_template = publisher.get_template_by_name(template_name)
+        item.properties["publish_template"] = publish_template
+        if not publish_template and not item.parent.properties.get("work_template"):
+            accepted = False
+
+        self.logger.info(
+            "Motion Builder '%s' plugin accepted the current Motion Builder session." %
+            (self.name,)
+        )
+        return {
+            "accepted": accepted,
+            "checked": True
+        }
+
+    def validate(self, settings, item):
+        """
+        Validates the given item to check that it is ok to publish. Returns a
+        boolean to indicate validity.
+
+        :param settings: Dictionary of Settings. The keys are strings, matching
+            the keys returned in the settings property. The values are `Setting`
+            instances.
+        :param item: Item to process
+        :returns: True if item is valid, False otherwise.
+        """
+
+        publisher = self.parent
+        path = _session_path()
+
+        if not path:
+            # the session still requires saving. provide a save button.
+            # validation fails.
+            self.logger.error(
+                "The Motion Builder session has not been saved.",
+                extra=_get_save_as_action()
+            )
+            return False
+
+        # get the path in a normalized state. no trailing separator,
+        # separators are appropriate for current os, no double separators,
+        # etc.
+        sgtk.util.ShotgunPath.normalize(path)
+
+        # get the publish name for this file path. this will ensure we get a
+        # consistent publish name when looking up existing publishes.
+        publish_name = publisher.util.get_publish_name(path)
+
+        # see if there are any other publishes of this path with a status.
+        # Note the name, context, and path *must* match the values supplied to
+        # register_publish in the publish phase in order for this to return an
+        # accurate list of previous publishes of this file.
+        publishes = publisher.util.get_conflicting_publishes(
+            item.context,
+            path,
+            publish_name,
+            filters=["sg_status_list", "is_not", None]
+        )
+
+        if publishes:
+            conflict_info = (
+                "If you continue, these conflicting publishes will no longer "
+                "be available to other users via the loader:<br>"
+                "<pre>%s</pre>" % (pprint.pformat(publishes),)
+            )
+            self.logger.warn(
+                "Found %s conflicting publishes in Shotgun" %
+                (len(publishes),),
+                extra={
+                    "action_show_more_info": {
+                        "label": "Show Conflicts",
+                        "tooltip": "Show the conflicting publishes in Shotgun",
+                        "text": conflict_info
+                    }
+                }
+            )
+
+        # ---- check the session against any attached work template
+
+        # get the path in a normalized state. no trailing separator,
+        # separators are appropriate for current os, no double separators,
+        # etc.
+        path = sgtk.util.ShotgunPath.normalize(path)
+
+        # if the session item has a known work template, see if the path
+        # matches. if not, warn the user and provide a way to save the file to
+        # a different path
+        work_template = item.properties.get("work_template")
+        if work_template:
+            if not work_template.validate(path):
+                self.logger.warning(
+                    "The current session does not match the configured work "
+                    "file template.",
+                    extra={
+                        "action_button": {
+                            "label": "Save File",
+                            "tooltip": "Save the current Motion Builder session to a "
+                                       "different file name",
+                            # will launch wf2 if configured
+                            "callback": _get_save_as_action()
+                        }
+                    }
+                )
+            else:
+                self.logger.debug(
+                    "Work template configured and matches session file.")
+        else:
+            self.logger.debug("No work template configured.")
+
+        # if the file has a version number in it, see if the next version exists
+        next_version_path = publisher.util.get_next_version_path(path)
+        if next_version_path and os.path.exists(next_version_path):
+
+            # determine the next available version_number. just keep asking for
+            # the next one until we get one that doesn't exist.
+            while os.path.exists(next_version_path):
+                next_version_path = publisher.util.get_next_version_path(
+                    next_version_path)
+
+            # now extract the version number of the next available to display
+            # to the user
+            version = publisher.util.get_version_number(next_version_path)
+
+            self.logger.error(
+                "The next version of this file already exists on disk.",
+                extra={
+                    "action_button": {
+                        "label": "Save to v%s" % (version,),
+                        "tooltip": "Save to the next available version number, "
+                                   "v%s" % (version,),
+                        "callback": lambda: _save_session(next_version_path)
+                    }
+                }
+            )
+            return False
+
+        # ---- populate the necessary properties and call base class validation
+
+        # populate the publish template on the item if found
+        publish_template_setting = settings.get("Publish Template")
+        publish_template = publisher.engine.get_template_by_name(
+            publish_template_setting.value)
+        if publish_template:
+            item.properties["publish_template"] = publish_template
+
+        # set the session path on the item for use by the base plugin validation
+        # step. NOTE: this path could change prior to the publish phase.
+        item.properties["path"] = path
+
+        # run the base class validation
+        return super(MotionBuilderSessionPublishPlugin, self).validate(settings, item)
+
+    def publish(self, settings, item):
+        """
+        Executes the publish logic for the given item and settings.
+
+        :param settings: Dictionary of Settings. The keys are strings, matching
+            the keys returned in the settings property. The values are `Setting`
+            instances.
+        :param item: Item to process
+        """
+
+        publisher = self.parent
+
+        # get the path in a normalized state. no trailing separator, separators
+        # are appropriate for current os, no double separators, etc.
+        path = sgtk.util.ShotgunPath.normalize(_session_path())
+
+        # ensure the session is saved
+        _save_session(path)
+
+        # update the item with the saved session path
+        item.properties["path"] = path
+
+        # let the base class register the publish
+        super(MotionBuilderSessionPublishPlugin, self).publish(settings, item)
+
+    def finalize(self, settings, item):
+        """
+        Execute the finalization pass. This pass executes once all the publish
+        tasks have completed, and can for example be used to version up files.
+
+        :param settings: Dictionary of Settings. The keys are strings, matching
+            the keys returned in the settings property. The values are `Setting`
+            instances.
+        :param item: Item to process
+        """
+
+        # do the base class finalization
+        super(MotionBuilderSessionPublishPlugin, self).finalize(settings, item)
+
+        # bump the session file to the next version
+        self._save_to_next_version(item.properties["path"], item, _save_session)
+
+def _session_path():
+    """
+    Return the path to the current session
+    :return:
+    """
+    path = mb_app.FBXFileName
+    if isinstance(path, unicode):
+        path = path.encode("utf-8")
+
+    return path
+
+
+def _save_session(path):
+    """
+    Save the current session to the supplied path.
+    """
+
+    mb_app.FileSave(path)
+
+
+def _get_save_as_action():
+    """
+
+    Simple helper for returning a log action dict for saving the session
+    """
+    return {
+        "action_button": {
+            "label": "Save As...",
+            "tooltip": "Save the current session",
+            "callback": lambda: _save_session(_session_path())
+        }
+    }
