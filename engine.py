a6d5de30ddaed1cfd99150c42fcac65237fe6147
Manne Ohrstrom <manne.ohrstrom@autodesk.com>
2019-03-22 22:47:07 +0000

    support for 2019

--- engine.py
+++ engine.py
@@ -39,8 +39,13 @@ def tank_mobu_exception_trap(ex_cls, ex, tb):
 
     # now output it    
     try:
-        from PySide import QtGui
-        QtGui.QMessageBox.critical(None, "Python Exception Raised", error_message)
+        from tank.util.qt_importer import QtImporter
+        qt = QtImporter()
+        qt.QtGui.QMessageBox.critical(
+            None, 
+            "Python Exception Raised", 
+            error_message
+        )
     except:
         try:
             print str(error_message)
@@ -59,7 +64,7 @@ class MotionBuilderEngine(tank.platform.Engine):
 
             {
                 "name": "Motionbuilder",
-                "version": "180000",
+                "version": "18000.0",
             }
 
         and of following form on an error preventing the version identification.
@@ -89,61 +94,31 @@ class MotionBuilderEngine(tank.platform.Engine):
             raise tank.TankError("The Motionbuilder engine needs at least a project in the context "
                                  "in order to start! Your context: %s" % self.context)
 
-        # import pyside QT UI libraries
-        self._init_pyside()
+        MOTIONBUILDER_2018_VERSION = 18000.0
+
+        if FBSystem().Version < MOTIONBUILDER_2018_VERSION:
+            # older versions are missing image plugins, hotpatch them in.
+            self._add_qt470_image_format_plugins_to_library_path()
 
         # motionbuilder doesn't have good exception handling, so install our own trap
         sys.excepthook = tank_mobu_exception_trap
 
-    def _init_pyside(self):
+    def _add_qt470_image_format_plugins_to_library_path(self):
         """
-        Handles the pyside init
+        Explicitly add image format plugins for qt 4.70/vs2010 compile
+
+        This is to patch an issue on older versions of Mobu where
+        these plugins are missing from the version of PySide that is
+        shipped with the DCC.
         """
+        from tank.platform.qt import QtCore
 
-        pyside_folder = self.__get_pyside_folder()
-
-        # first see if pyside is already present - in that case skip!
-        try:
-            from PySide import QtGui
-        except:
-            # fine, we don't expect pyside to be present just yet
-            self.log_debug("PySide not detected - it will be added to the setup now...")
-        else:
-            # pyside was found. we will ensure the image format plugins are
-            # available
-            self.log_debug("PySide detected - the existing version will be used.")
-            self._add_image_format_plugins_to_library_path(pyside_folder)
-            return
-
-        if sys.platform == "win32":
-            pyside_path = os.path.join(self.disk_location, "resources", pyside_folder, "python")
-            sys.path.append(pyside_path)
-            dll_path = os.path.join(self.disk_location, "resources", pyside_folder, "lib")
-            path = os.environ.get("PATH", "")
-            path += ";%s" % dll_path
-            os.environ["PATH"] = path
-
-        else:
-            self.log_error("Unknown platform - cannot initialize PySide!")
-
-        # now try to import it
-        try:
-            from PySide import QtCore
-        except Exception, e:
-            self.log_error("PySide could not be imported! Apps using pyside will not "
-                           "operate correctly! Error reported: %s" % e)
-        else:
-            self._add_image_format_plugins_to_library_path(pyside_folder)
-
-    def _add_image_format_plugins_to_library_path(self, pyside_folder):
-        """
-        Add image format plugins to path so they can be loaded and used
-
-        :param: pyside_folder Filesystem location where the plugins live
-        """
-        from PySide import QtCore
-
-        plugin_path = os.path.join(self.disk_location, "resources", pyside_folder, "qt_plugins")
+        plugin_path = os.path.join(
+            self.disk_location, 
+            "resources", 
+            "qt470_win64_vs2010", 
+            "qt_plugins"
+        )
 
         self.log_debug(
             "Adding support for various image formats via qplugins."
@@ -159,7 +134,7 @@ class MotionBuilderEngine(tank.platform.Engine):
         
         :returns: QWidget if found or None if not
         """
-        from PySide import QtGui
+        from tank.platform.qt import QtGui
         
         # get all top level windows:
         top_level_windows = QtGui.QApplication.topLevelWidgets()
@@ -172,26 +147,6 @@ class MotionBuilderEngine(tank.platform.Engine):
                 return w
         
         return None
-
-    def __get_pyside_folder(self):
-        """
-        Get the correct pyside folder name according to the 
-        correct Motionbuilder version.
-
-        :returns: String of the correct pyside distribution folder name (not the entire path)
-        """
-        version = FBSystem().Version
-        folder = ''
-
-        # MotionBuilder 2012 and 2013 use a qt compiled with vs2008 (even though mobu 2013 itself is compiled with 2010).
-        # Use a dumpbin /headers on MoBu's qtcore4.dll to see which linker version was used, since versioning can
-        # easily get confusing.
-        if version < 14000:
-            folder = "pyside112_py26_qt470_win64_vs2008"
-        else:
-            folder = "pyside112_py26_qt470_win64_vs2010"
-
-        return folder
 
     def post_app_init(self):
         # default menu name is Shotgun but this can be overriden
