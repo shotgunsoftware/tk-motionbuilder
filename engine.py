f0a92eab5e9aa0cd83fefedfc9d79e7a8b791437
Manne Öhrström <manne.ohrstrom@autodesk.com>
2019-04-05 14:45:08 +0100

    SG-10370 - Support for 2019 + Misc changes (#9)
    - Note: This version of the engine is not compatible with versions before 2014.
    - Note: Engine now requires core v0.18+
    
    - Adds support for mobu 2019, PySide2 and cleans up the engine payload. (as expressed in #8)
    - Removes binary files (qt/pyside distros), thereby reducing the size of the engine drastically.
    - Adds context switching support, as outlined in #7
    - Updates to 0.18 logging
    - Removes deprecated config params debug_logging and use_sgtk_as_menu_name
    - Updates tank to sgtk

--- engine.py
+++ engine.py
@@ -1,11 +1,11 @@
 # Copyright (c) 2013 Shotgun Software Inc.
-# 
+#
 # CONFIDENTIAL AND PROPRIETARY
-# 
-# This work is provided "AS IS" and subject to the Shotgun Pipeline Toolkit 
+#
+# This work is provided "AS IS" and subject to the Shotgun Pipeline Toolkit
 # Source Code License included in this distribution package. See LICENSE.
-# By accessing, using, copying or modifying this work you indicate your 
-# agreement to the Shotgun Pipeline Toolkit Source Code License. All rights 
+# By accessing, using, copying or modifying this work you indicate your
+# agreement to the Shotgun Pipeline Toolkit Source Code License. All rights
 # not expressly granted therein are reserved by Shotgun Software Inc.
 
 """
@@ -15,40 +15,45 @@ A MotionBuilder engine for Shotgun.
 
 import os
 import sys
-
-# tank libs
-import tank
+import sgtk
+import logging
 
 # application libs
 from pyfbsdk import FBMessageBox
 from pyfbsdk import FBSystem
 
+
 # custom exception handler for motion builder
-def tank_mobu_exception_trap(ex_cls, ex, tb):
-    # careful about infinite loops here - 
+def sgtk_mobu_exception_trap(ex_cls, ex, tb):
+    # careful about infinite loops here -
     # MUST NOT RAISE EXCEPTIONS :)
-    
+
     # assemble message
     error_message = "Could not format error message"
     try:
         import traceback
         tb_str = "\n".join(traceback.format_tb(tb))
         error_message = "A Python Exception was Caught!\n\nDetails: %s\nError Type: %s\n\nTraceback:\n%s" % (ex, ex_cls, tb_str)
-    except:
+    except Exception:
         pass
 
-    # now output it    
+    # now output it
     try:
-        from PySide import QtGui
-        QtGui.QMessageBox.critical(None, "Python Exception Raised", error_message)
+        from sgtk.util.qt_importer import QtImporter
+        qt = QtImporter()
+        qt.QtGui.QMessageBox.critical(
+            None,
+            "Python Exception Raised",
+            error_message
+        )
     except:
         try:
             print str(error_message)
         except:
             pass
-        
 
-class MotionBuilderEngine(tank.platform.Engine):
+
+class MotionBuilderEngine(sgtk.platform.Engine):
 
     @property
     def host_info(self):
@@ -59,7 +64,7 @@ class MotionBuilderEngine(tank.platform.Engine):
 
             {
                 "name": "Motionbuilder",
-                "version": "180000",
+                "version": "18000.0",
             }
 
         and of following form on an error preventing the version identification.
@@ -80,72 +85,72 @@ class MotionBuilderEngine(tank.platform.Engine):
             pass
 
         return host_info
-    
+
+    @property
+    def context_change_allowed(self):
+        """
+        Whether the engine allows a context change without the need for a restart.
+        """
+        return True
+
     def init_engine(self):
-        self.log_debug("%s: Initializing..." % self)
-        
+        self.logger.debug("%s: Initializing..." % self)
+
         if self.context.project is None:
             # must have at least a project in the context to even start!
-            raise tank.TankError("The Motionbuilder engine needs at least a project in the context "
+            raise sgtk.TankError("The Motionbuilder engine needs at least a project in the context "
                                  "in order to start! Your context: %s" % self.context)
 
-        # import pyside QT UI libraries
-        self._init_pyside()
-
         # motionbuilder doesn't have good exception handling, so install our own trap
-        sys.excepthook = tank_mobu_exception_trap
+        sys.excepthook = sgtk_mobu_exception_trap
 
-    def _init_pyside(self):
+    def post_app_init(self):
         """
-        Handles the pyside init
+        Executes once all apps have been initialized
         """
+        MOTIONBUILDER_2018_VERSION = 18000.0
+        if FBSystem().Version < MOTIONBUILDER_2018_VERSION:
+            # older versions are missing image plugins, hotpatch them in.
+            self._add_qt470_image_format_plugins_to_library_path()
 
-        pyside_folder = self.__get_pyside_folder()
+        self._initialize_menu()
 
-        # first see if pyside is already present - in that case skip!
-        try:
-            from PySide import QtGui
-        except:
-            # fine, we don't expect pyside to be present just yet
-            self.log_debug("PySide not detected - it will be added to the setup now...")
-        else:
-            # pyside was found. we will ensure the image format plugins are
-            # available
-            self.log_debug("PySide detected - the existing version will be used.")
-            self._add_image_format_plugins_to_library_path(pyside_folder)
-            return
-
-        if sys.platform == "win32":
-            pyside_path = os.path.join(self.disk_location, "resources", pyside_folder, "python")
-            sys.path.append(pyside_path)
-            dll_path = os.path.join(self.disk_location, "resources", pyside_folder, "lib")
-            path = os.environ.get("PATH", "")
-            path += ";%s" % dll_path
-            os.environ["PATH"] = path
-
-        else:
-            self.log_error("Unknown platform - cannot initialize PySide!")
-
-        # now try to import it
-        try:
-            from PySide import QtCore
-        except Exception, e:
-            self.log_error("PySide could not be imported! Apps using pyside will not "
-                           "operate correctly! Error reported: %s" % e)
-        else:
-            self._add_image_format_plugins_to_library_path(pyside_folder)
-
-    def _add_image_format_plugins_to_library_path(self, pyside_folder):
+    def post_context_change(self, old_context, new_context):
         """
-        Add image format plugins to path so they can be loaded and used
+        Handles post-context-change requirements.
 
-        :param: pyside_folder Filesystem location where the plugins live
+        :param old_context: The sgtk.context.Context being switched away from.
+        :param new_context: The sgtk.context.Context being switched to.
         """
-        from PySide import QtCore
+        self.logger.debug("tk-motionbuilder context changed to %s", str(new_context))
+        self._menu_generator.destroy_menu()
+        self._initialize_menu()
 
-        plugin_path = os.path.join(self.disk_location, "resources", pyside_folder, "qt_plugins")
+    def destroy_engine(self):
+        """
+        Uninitialize engine state
+        """
+        self.logger.debug('%s: Destroying...' % self)
+        self._menu_generator.destroy_menu()
 
-        self.log_debug(
+    def _add_qt470_image_format_plugins_to_library_path(self):
+        """
+        Explicitly add image format plugins for qt 4.70/vs2010 compile
+
+        This is to patch an issue on older versions of Mobu where
+        these plugins are missing from the version of PySide that is
+        shipped with the DCC.
+        """
+        from sgtk.platform.qt import QtCore
+
+        plugin_path = os.path.join(
+            self.disk_location,
+            "resources",
+            "qt470_win64_vs2010",
+            "qt_plugins"
+        )
+
+        self.logger.debug(
             "Adding support for various image formats via qplugins."
             "Plugin path: %s" % (plugin_path,)
         )
@@ -156,72 +161,53 @@ class MotionBuilderEngine(tank.platform.Engine):
         Find the main Motionbuilder window/QWidget.  This
         will be used as the parent for all dialogs created
         by show_modal or show_dialog
-        
+
         :returns: QWidget if found or None if not
         """
-        from PySide import QtGui
-        
+        from sgtk.platform.qt import QtGui
+
         # get all top level windows:
         top_level_windows = QtGui.QApplication.topLevelWidgets()
-        
+
         # from this list, find the main application window.
         for w in top_level_windows:
-            if (type(w) == QtGui.QWidget        # window is always QWidget 
-                and len(w.windowTitle()) > 0    # window always has a title/caption
-                and w.parentWidget() == None):  # parent widget is always None
+            if type(w) == QtGui.QWidget and len(w.windowTitle()) > 0 and w.parentWidget() is None:
                 return w
-        
+
         return None
 
-    def __get_pyside_folder(self):
+    def _initialize_menu(self):
         """
-        Get the correct pyside folder name according to the 
-        correct Motionbuilder version.
-
-        :returns: String of the correct pyside distribution folder name (not the entire path)
+        Creates a new motionbuilder menu
+        to reflect currently loaded apps.
         """
-        version = FBSystem().Version
-        folder = ''
-
-        # MotionBuilder 2012 and 2013 use a qt compiled with vs2008 (even though mobu 2013 itself is compiled with 2010).
-        # Use a dumpbin /headers on MoBu's qtcore4.dll to see which linker version was used, since versioning can
-        # easily get confusing.
-        if version < 14000:
-            folder = "pyside112_py26_qt470_win64_vs2008"
-        else:
-            folder = "pyside112_py26_qt470_win64_vs2010"
-
-        return folder
-
-    def post_app_init(self):
-        # default menu name is Shotgun but this can be overriden
-        # in the configuration to be Sgtk in case of conflicts
-        menu_name = "Shotgun"
-        if self.get_setting("use_sgtk_as_menu_name", False):
-            menu_name = "Sgtk"
-            
-        tk_motionbuilder = self.import_module("tk_motionbuilder")                
-        self._menu_generator = tk_motionbuilder.MenuGenerator(self, menu_name)
+        tk_motionbuilder = self.import_module("tk_motionbuilder")
+        self._menu_generator = tk_motionbuilder.MenuGenerator(self, "Shotgun")
         self._menu_generator.create_menu()
 
-    def destroy_engine(self):
-        self.log_debug('%s: Destroying...' % self)
-        self._menu_generator.destroy_menu()
+    def _emit_log_message(self, handler, record):
+        """
+        Called by the engine to log messages in Mobu script editor.
+        All log messages from the toolkit logging namespace will be passed to this method.
+        :param handler: Log handler that this message was dispatched from.
+                        Its default format is "[levelname basename] message".
+        :type handler: :class:`~python.logging.LogHandler`
+        :param record: Standard python logging record.
+        :type record: :class:`~python.logging.LogRecord`
+        """
+        # Give a standard format to the message:
+        #     Shotgun <basename>: <message>
+        # where "basename" is the leaf part of the logging record name,
+        # for example "tk-multi-shotgunpanel" or "qt_importer".
+        if record.levelno < logging.INFO:
+            formatter = logging.Formatter("Debug: Shotgun %(basename)s: %(message)s")
+        else:
+            formatter = logging.Formatter("Shotgun %(basename)s: %(message)s")
 
-    def log_debug(self, msg):
-        if self.get_setting("debug_logging", False):
+        msg = formatter.format(record)
+
+        if record.levelno < logging.ERROR:
             print msg
-
-    def log_info(self, msg):
-        msg = "Shotgun: %s" % msg
-        print msg
-
-    def log_error(self, msg):
-        FBMessageBox( "Shotgun Error",  str(msg), "OK" )
-
-    def log_warning(self, msg):
-        msg = "Shotgun Warning: %s" % msg
-        print msg
-
-
-
+        else:
+            # for errors, pop up a modal msgbox
+            FBMessageBox("Shotgun Error", str(msg), "OK")
